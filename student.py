150136c6454ec4963aeec3f77f7ccac130273e4c
fayoue <161594383+fayoue@users.noreply.github.com>
2026-09-22 12:44:36 +0300

    Task 2

---
+++ student.py
@@ -0,0 +1,14 @@
+
+#  University Student Information System
+
+# Store student information in variables
+student_name = "Faith Muthini"
+registration_number = "BSCIT-05-O201/2024"
+course = "Bachelor of Information Technology"
+year_of_study = 3.1
+
+# Display student information
+print(f"Student Name: {student_name}")
+print(f"Registration Number: {registration_number}")
+print(f"Course: {course}")
+print(f"Year of Study: {year_of_study}")
